33b4a0f7369ffd0c725c2b7001dfe708712d0561
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-06-17 22:01:03 +0900

    Fix routing settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule, MdCheckboxModule} from '@angular/material';
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import 'hammerjs';
-import { TimeLineComponent } from './time-line/time-line.component';
-import { HeaderComponent } from './header/header.component';
+import {TimeLineComponent} from './time-line/time-line.component';
+import {HeaderComponent} from './header/header.component';
+import {RouterModule} from '@angular/router';
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     TimeLineComponent,
     HeaderComponent
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MdButtonModule,
-    MdCheckboxModule
-  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
