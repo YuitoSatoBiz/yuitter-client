cd96853de58b9348c7e69706833ded17228c04cd
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2017-06-21 21:16:09 +0900

    add cookie service

--- src/app/components/time-line/time-line.component.ts
+++ src/app/components/time-line/time-line.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Account} from '../../classes/account'
 import {MemberService} from '../../services/member-service/member.service';
 import {Member} from 'app/classes/member';
+import {CookieService} from 'angular2-cookie/core';
 
 @Component({
   selector: 'app-time-line',
@@ -14,7 +15,7 @@ export class TimeLineComponent implements OnInit {
   accounts: Account[];
   currentAccount: Account;
 
-  constructor(private memberService: MemberService) {
+  constructor(private memberService: MemberService, private cookieService: CookieService) {
   }
 
   ngOnInit() {
@@ -28,5 +29,15 @@ export class TimeLineComponent implements OnInit {
         this.accounts = this.currentMember.accounts;
         console.log(member.accounts);
       });
+  }
+
+  onClick(account: Account): void {
+    this.currentAccount = account;
+    this.cookieService.remove('accountId');
+    console.log(this.cookieService.getObject('accountId'));
+    console.log(this.cookieService.getAll());
+    console.log(this.cookieService.get('PLAY_SESSION'));
+    this.cookieService.put('accountId', account.accountId.toString());
+    console.log(this.cookieService.get('accountId'));
   }
 }
