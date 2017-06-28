723cf534d26a8f2c4ceb40d590a0766d3dd74cb2
佐藤 有斗 <yuito.sato@bizreach.co.jp>
2017-06-29 01:52:27 +0900

    update tweet form

--- src/app/components/tweet-list/tweet-list.component.ts
+++ src/app/components/tweet-list/tweet-list.component.ts
@@ -15,25 +15,12 @@ export class TweetListComponent implements OnInit {
   @Input() account: Account;
   @Input() accounts: Account[];
   @Input() currentAccountIds: number[];
-  @Output() selected: number;
+  tweet: Tweet;
   tweets: Tweet[];
   accountIds: number[];
+  error: string;
   options: Array<{accountId: number, name: string}>;
-
-  foods = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'}
-  ];
-
-
-  // setting this is the key to initial select.
   selectedValue: number;
-
-  static handleError(error: any): Promise<any> {
-    console.error('An error occurred', error);
-    return Promise.reject(error.message || error);
-  }
 
   constructor(private tweetService: TweetService) {
   }
@@ -41,10 +28,11 @@ export class TweetListComponent implements OnInit {
   getTweets(): void {
     this.tweetService.list(this.account.accountId).then(tweets =>
       this.tweets = tweets
-    ).catch(TweetListComponent.handleError);
+    )
   }
 
   ngOnInit() {
+    this.tweet = new Tweet(null, null, null, null, null);
     this.getTweets();
     this.accountIds = this.accounts.map(a => a.accountId);
     this.options = [
@@ -55,18 +43,22 @@ export class TweetListComponent implements OnInit {
   }
 
   onSubmit(tweetForm: FormGroup): void {
-    const accountId = tweetForm.value.accountId;
-    let accountIds: number[];
-    if (accountId >= 0) {
-      accountIds = [accountId];
+    if (!(tweetForm.value.tweetText.replace(/\s+/g, '').replace(/\r?\n/g, ''))) {
+      this.error = '値が不正です。'
     } else {
-      accountIds = this.accountIds;
+      const accountId = tweetForm.value.accountId;
+      let accountIds: number[];
+      if (accountId >= 0) {
+        accountIds = [accountId];
+      } else {
+        accountIds = this.accountIds;
+      }
+      this.tweetService.create(tweetForm.value.tweetText, accountIds)
+        .then(tweet =>
+          this.tweets.unshift(tweet)
+        );
     }
-    this.tweetService.create(tweetForm.value.tweetText, accountIds)
-      .then(tweet =>
-        this.tweets.unshift(tweet)
-      );
-    tweetForm.reset({accountId: this.account.accountId});
+    tweetForm.reset({ accountId: this.account.accountId });
     this.selectedValue = this.options[0].accountId;
   }
 }
